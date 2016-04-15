00769828256059e260e902a3612be4031b4f04e6
Nguyen Anh Tuan <anhtuan.nguyen@CJCGVVIETNAM.net>
2016-04-15 11:20:06 +0700

    remove duplicate function

--- src/Utility/functions.php
+++ src/Utility/functions.php
@@ -23,24 +23,3 @@ function enqueue($to, $cc = null, $bcc = null, $reply_to = null, array $data, ar
 {
     return \Cake\ORM\TableRegistry::get('EmailQueue.EmailQueue')->enqueue($to, $cc, $bcc, $reply_to, $data, $options);
 }
-
-/**
- * Stores a new email message in the queue.
- *
- * @param mixed|array $to           email or array of emails as recipients
- * @param array $data    associative array of variables to be passed to the email template
- * @param array $options list of options for email sending. Possible keys:
- *
- * - subject : Email's subject
- * - send_at : date time sting representing the time this email should be sent at (in UTC)
- * - template :  the name of the element to use as template for the email message
- * - layout : the name of the layout to be used to wrap email message
- * - format: Type of template to use (html, text or both)
- * - config : the name of the email config to be used for sending
- *
- * @return bool
- */
-function enqueue($to, array $data, array $options = [])
-{
-    return \Cake\ORM\TableRegistry::get('EmailQueue.EmailQueue')->enqueue($to, null, null, null, $data, $options);
-}
